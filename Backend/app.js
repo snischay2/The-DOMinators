3370a25e8120eb4a66e274ee5222174f3c7d078f
Nischay Sharma <snischay192006@gmail.com>
2025-02-20 00:03:42 +0530

    The backend now serves the frontend

--- Backend/app.js
+++ Backend/app.js
@@ -8,10 +8,14 @@ const mongoose = require('mongoose');
 const attendanceRoutes = require("./routes/attendanceRoutes");
 const routes = require('./routes');
 
+const path = require("path");
+
 
 require('./conn/conn');
 app.use(cors());
 app.use(express.json());
+
+const _dirname = path.resolve();
 
 
 
@@ -20,7 +24,10 @@ app.use("/api/v1", contact);
 app.use("/api/v1/attendance", attendanceRoutes);
 app.use("/api" ,routes);
 
-
+app.use(express.static(path.join(_dirname, "/Project_1/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "Project_1", "dist", "index.html"));
+});
 
 app.listen("1000", () => {
     console.log("Server at port 1000");
